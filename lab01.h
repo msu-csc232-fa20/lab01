1116e184026898ae4bdf60d23240821d77587a67
Jim Daehn <jdaehn@missouristate.edu>
2020-08-24 18:45:45 -0500

    LAB01 - Initial import of lab01.h

---
+++ lab01.h
@@ -0,0 +1,17 @@
+/**
+ * CSC232 - Data Structures
+ * Missouri State University, Fall 2020
+ *
+ * @file    lab01.h
+ * @author  Jim Daehn <jdaehn@missouristate.edu>
+ * @brief   Macros for use in lab01.
+ */
+
+#ifndef CSC232_LAB01_H
+#define CSC232_LAB01_H
+
+#define FALSE 0
+#define TRUE 1
+#define EXECUTE_BLOCK FALSE
+
+#endif // CSC232_LAB01_H
